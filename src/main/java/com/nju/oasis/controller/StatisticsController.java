a3dc1c32df12d50c41e029ae7d0e32232af2bf77
Xing-Chi Liu <771788576@qq.com>
2020-02-26 19:56:37 +0800

    small fix

--- src/main/java/com/nju/oasis/controller/StatisticsController.java
+++ src/main/java/com/nju/oasis/controller/StatisticsController.java
@@ -5,6 +5,8 @@ import com.nju.oasis.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -20,7 +22,8 @@ public class StatisticsController {
     AuthorService authorService;
 
     @GetMapping("/top/author")
-    public List<AuthorStatistics> getAuthorsWithMaxDocumentCount(int num){
+    @ResponseBody
+    public List<AuthorStatistics> getAuthorsWithMaxDocumentCount(@RequestParam("num") int num){
         return authorService.getAuthorsMaxDocumentCount(num);
     }
 
